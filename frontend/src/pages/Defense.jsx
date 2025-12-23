7ae0c58090bcca342b99308fda87cff5ff35bff0
naitikagarwal-12 <naitikagarwal.938@gmail.com>
2025-12-23 19:02:38 +0530

    military

--- frontend/src/pages/Defense.jsx
+++ frontend/src/pages/Defense.jsx
@@ -10,15 +10,15 @@ const Defense = () => {
   const navigate = useNavigate();
   return (
     <>
-      <section className="bg-[#b7987d] text-black p-5">
+      <section id="defence" className="bg-[#b7987d] text-black p-5">
         <div className="flex flex-col justify-center items-center">
           <h1 className="text-4xl sm:text-5xl mt-4 lg:text-6xl saira-stencil-one">
-            Defense System
+            Defence System
           </h1>
         </div>
 
         <div className="flex justify-center items-center saira-extra-condensed font-bold">
-          <div className="grid grid-cols-1 md:grid-cols-3 my-7 mt-15 w-[65%] sm:w-[55%] md:w-[85%] gap-2">
+          <div className="grid grid-cols-1 md:grid-cols-3 my-7 mt-15 w-[65%] sm:w-[55%] md:w-[85%] gap-5">
             <MilitaryCards
               imgClass={"max-h-40 w-auto"}
               image={navy}
