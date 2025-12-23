a5eb2b84bf2d78307df5364686b3997b916da611
naitikagarwal-12 <naitikagarwal.938@gmail.com>
2025-12-24 00:12:44 +0530

    military

--- frontend/src/components/DefenseSuite.jsx
+++ frontend/src/components/DefenseSuite.jsx
@@ -1,15 +1,12 @@
+import { useEffect } from "react";
 import Header from "./Header";
 
 const DefenseSuite = () => {
-  return (
-    <div className="bg-[#27395f]">
-      <Header />
-      <iframe
-        src={`${import.meta.env.VITE_DEFENSE_API}`}
-        style={{ width: "100%", height: "100vh", border: "none" }}
-      />
-    </div>
-  );
+    useEffect(() => {
+    window.location.href = "https://defense-suite.onrender.com/";
+  }, []);
+
+  return null;
 };
 
 export default DefenseSuite;
