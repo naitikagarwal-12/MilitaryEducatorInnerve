d28aa7ba903081a0bb83dd8e1a0f6429c5b21a98
Pramod <pcj3.141592@gmail.com>
2025-12-23 17:46:31 +0530

    changed cors

--- backend/userManagement/src/server.js
+++ backend/userManagement/src/server.js
@@ -14,17 +14,9 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://military-educator-innerve.vercel.app",
-];
-
 app.use(
   cors({
-    origin: (origin, cb) => {
-      if (!origin || allowedOrigins.includes(origin)) cb(null, true);
-      else cb(new Error("CORS blocked"));
-    },
+    origin: "https://military-educator-innerve.vercel.app",
     credentials: true,
   })
 );
